58501315465dc5d8513f324a5748b1a4cf8b8442
dwdevs <don_w@hotmail.co.uk>
2025-02-02 23:49:30 +0400

    Tested NGINX and fixed server pid issue

--- rails-server.sh
+++ rails-server.sh
@@ -7,7 +7,7 @@ bundle exec rails new . --force --database=postgresql
 cp -av ../database.yml ./config/
 
 # Remove any pre-existing server.pid for Rails - Fixes start-up issue with existing pid leftover
-rm -f ./app/tmp/pids/server.pid
+rm -f /usr/src/app/tmp/pids/server.pid
 
 # Start rails server
 bundle exec rails server -b 0.0.0.0 -p 3000
